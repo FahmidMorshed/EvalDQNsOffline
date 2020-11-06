e4172e7568b4f65fc3f5303233eaaf33fa57cbc1
tshahri <tshahri@ncsu.edu>
2020-11-06 18:38:36 -0500

    Setup.py for gridworld

---
+++ src/gym_gridworld/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Third party
+from setuptools import setup
+
+setup(name='gym_gridworld',
+      version='0.0.1',
+      install_requires=['gym']#And any other dependencies required
+)
